80da836d5f4be7ab3ba62ef5eaa2f34ce38b8b06
Roland Stedy <stedyroland@gmail.com>
2024-10-30 06:12:38 +0300

    Get Started section

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,8 +1,18 @@
+import styles from '@/styles'
 import React from 'react'
 
-function StartSteps() {
+function StartSteps({number, text}) {
   return (
-    <div>StartSteps</div>
+    <div className={`${styles.flexCenter} flex-row`}>
+      <div className={`${styles.flexCenter} h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]`}>
+        <p className='font-bold text-[20px] text-white'>
+          0{number}
+        </p>
+      </div>
+      <p className='flex-1 ml-[30px] font-normal text-[18px] text-[#B0B0B0] leading-[32px]'>
+        {text}
+      </p>
+    </div>
   )
 }
 
