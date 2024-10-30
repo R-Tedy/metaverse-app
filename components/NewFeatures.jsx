3f21d3355aee52fa398c5aecbd272ae74aed408b
Roland Stedy <stedyroland@gmail.com>
2024-10-30 07:03:27 +0300

    New features done

--- components/NewFeatures.jsx
+++ components/NewFeatures.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import styles from "@/styles"
 
-function NewFeatures() {
+function NewFeatures({imgUrl, title, subtitle}) {
   return (
-    <div>NewFeatures</div>
+    <div className={`${styles.flexStart} flex-col gap-4 sm:max-w-[250px] min-w-[225px]`}>
+      <div className={`${styles.flexCenter} h-[70px] w-[70px] rounded-[24px] bg-[#323f5d]`}>
+        <img
+          src={imgUrl}
+          alt={title}
+          className="w-1/2 h-1/2 object-contain"
+        />
+      </div>
+      <h2 className="text-white font-bold text-[20px]">{title}</h2>
+      <p className="font-normal text-[#B0B0B0] leading-[32px] text-[18px] flex-1 flex">{subtitle}</p>
+    </div>
   )
 }
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+'use client'
+
+import { motion } from "framer-motion"
+import { staggerContainer,fadeIn, planetVariants } from "@/utils/motion"
+import { StartSteps, TitleText, TypingText } from "@/components"
+import { newFeatures } from "@/constants"
+import { NewFeatures } from "@/components"
+import styles from "@/styles"
+
 
 function WhatsNew() {
   return (
-    <div>WhatsNew</div>
+    <section className={`${styles.paddings} relative z-10`}>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{once: false, amount: 0.25}}
+        className={`flex flex-col ${styles.innerWidth} lg:flex-row mx-auto gap-8`}
+      >
+        <motion.div
+          variants={fadeIn('right', 'tween', 0.2, 1)}
+          className="flex-[0.75] flex flex-col justify-center"
+        >
+          <TypingText
+           title='| Whats New'
+          />
+          <TitleText
+            title={<>What's new about Metaversus</>}
+          />
+          <div className={`flex flex-row flex-wrap justify-between gap-[36px] mt-[31px]`}>
+            {newFeatures.map((feature, index)=>(
+              <NewFeatures
+                key={index}
+                {...feature}
+              />
+            ))}
+          </div>
+        </motion.div>
+        <motion.div
+          variants={planetVariants('right')}
+          className={`flex-1 ${styles.flexCenter}`}
+        >
+          <img 
+            src="/whats-new.png" 
+            alt="whats-new" 
+            className="h-[90%] w-[90%] object-contain"
+          />
+        </motion.div>
+      </motion.div>
+    </section>
   )
 }
 
